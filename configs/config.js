29975e85e75a290664321b0f09bb6a6a8cdf2512
sonyamrz <sofaelison@gmail.com>
2023-05-13 10:39:47 +0300

    1

---
+++ configs/config.js
@@ -0,0 +1,20 @@
+const MongoClient = require('mongodb').MongoClient;
+
+async function connectToMongoDB() {
+    try {
+      const uri = "mongodb://127.0.0.1:27017/RK";
+      const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      await client.connect();
+      console.log("Connected to MongoDB!");
+      const db = client.db("RK");
+      return db;
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      process.exit(1);
+    }
+  }
+  
+  module.exports = connectToMongoDB;
